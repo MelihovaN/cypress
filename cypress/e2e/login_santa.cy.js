e9335927d75c58c2385627c2aa89aa3176f07d0b
n.melihova <melihova.qa@gmail.com>
2023-03-13 20:21:21 +0200

    Added login fields tests

---
+++ cypress/e2e/login_santa.cy.js
@@ -0,0 +1,43 @@
+import { faker } from "@faker-js/faker";
+const registrationElements = require("../fixtures/pages/registrationSelectors.json");
+
+describe("Login page tests", () => {
+  beforeEach("login", () => {
+    //sign in to the account
+    cy.visit("/");
+    cy.pressClick(
+      ".layout-1__header-wrapper-fixed > .layout-1__header > .header > .header__items > a > .base--clickable > .header-item__text > .txt--med"
+    );
+  });
+  it("Invalid email and password", () => {
+    cy.get(registrationElements.nameField).type("melihovagmail.com");
+    cy.get(registrationElements.emailField).type(".");
+    cy.get(registrationElements.registrationButton).click();
+    cy.get(".frm-wrapper__top__error").contains("Некорректный email");
+    cy.get(".hint > .txt-secondary").contains("В форме допущены ошибки");
+  });
+  it("Unregistered user data", () => {
+    let newEmail = faker.internet.email();
+    let newPassword = faker.internet.password();
+    cy.get(registrationElements.nameField).type(newEmail);
+    cy.get(registrationElements.emailField).type(newPassword);
+    cy.get(registrationElements.registrationButton).click();
+    cy.get(".hint > .txt-secondary").contains(
+      "Мы не нашли пользователя с таким email"
+    );
+  });
+  it("Blank fields", () => {
+    cy.get(registrationElements.registrationButton).click();
+    cy.get(
+      ":nth-child(3) > .frm-wrapper__top > .frm-wrapper__top__error"
+    ).contains("Обязательное поле");
+    cy.get(".hint > .txt-secondary").contains("В форме допущены ошибки");
+  });
+
+  it("Valid email and password", () => {
+    cy.enterText(registrationElements.nameField, Cypress.env("email"));
+    cy.enterText(registrationElements.emailField, Cypress.env("password"));
+    cy.pressClick(registrationElements.registrationButton);
+    cy.contains("Уведомления").should("exist");
+  });
+});
